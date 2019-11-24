b0dd8249a151cb23ded8b826dd4a938637b75d97
Will Crawford <williambcrawf@gmail.com>
2019-11-24 18:15:06 -0500

    More initial changes

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace MovieQuery
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Movies}/{action=Index}/{id?}");
             });
         }
     }
